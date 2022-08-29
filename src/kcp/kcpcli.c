0ceb102e5f9219aa557eddbcb938afd32b8cba0b
Helly Guo <buffoonguo@gmail.com>
2022-08-29 16:19:09 +0800

    单线程只使用kcp库

---
+++ src/kcp/kcpcli.c
@@ -0,0 +1,49 @@
+#include "kcpdemo.h"
+
+static char *buf;
+
+void loop_send_kcp(udp_holder *holder) {
+  ikcpcb *kcp = holder->kcp;
+  // 固定休眠
+  usleep(1000);
+  // 获取底层数据包
+  fetch_buf(kcp);
+  // 获取时间戳并更新kcp
+  timestamp tsx = iclockX();
+  ikcp_update(kcp, tsx.curr_ms);
+  int rt = ikcp_waitsnd(kcp);
+  if (rt >= WND_SIZE) {
+#if __DEBUG
+    printf("%s too many wait msgs: %d packages\n", holder->name, rt);
+#endif
+    usleep(10 * 1000);
+    return;
+  }
+  // 获得时间戳并发送
+  char *p = buf;
+  p = encode64u(p, tsx.sec);
+  p = encode64u(p, tsx.nsec);
+  rt = ikcp_send(kcp, buf, DATA_SIZE);
+  // 强制刷新，确保第一时间发出，降低延时
+  if (rt < 0) {
+    return;
+  }
+  ikcp_flush(kcp);
+  rt = ikcp_waitsnd(kcp);
+  ++holder->count;
+#if __DEBUG
+  printf("%s sent: %d packages, %d packages waiting\n", holder->name,
+         holder->count, rt);
+#endif
+  // 校验退出
+  if (check_quit(holder)) {
+    printf("quit %s\n", holder->name);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  buf = malloc(DATA_SIZE);
+  test_kcp(1, loop_send_kcp);
+  free(buf);
+  return 0;
+}
---
+++ src/kcp/kcpsrv.c
@@ -0,0 +1,47 @@
+#include "kcpdemo.h"
+
+static IUINT64 *latencyStore;
+static char *buf;
+
+void echo_kcp(udp_holder *holder) {
+  ikcpcb *kcp = holder->kcp;
+  // 获取底层数据包
+  fetch_buf(kcp);
+  // 获取时间戳并更新kcp，从kcp读取数据
+  timestamp tsx = iclockX();
+  ikcp_update(kcp, tsx.curr_ms);
+  int rt = ikcp_recv(kcp, buf, DATA_SIZE);
+  if (rt < 0) {
+#if __DEBUG
+    printf("%s ikcp_recv failed: %d, %d|%d\n", holder->name, rt,
+           holder->kcp->nrcv_buf, holder->kcp->nrcv_que);
+#endif
+    return;
+  }
+  // 解析时间戳并计算延时
+  IUINT64 sec;
+  IUINT64 nsec;
+  const char *p = buf;
+  p = decode64u(p, &sec);
+  p = decode64u(p, &nsec);
+  latencyStore[holder->count++] =
+      (tsx.sec - sec) * 1000000000UL + tsx.nsec - nsec;
+#if __DEBUG
+  printf("%s received: %llu.%llu, current: %llu.%llu, %d packages\n",
+         holder->name, sec, nsec, tsx.sec, tsx.nsec, holder->count);
+#endif
+  // 校验退出
+  if (check_quit(holder)) {
+    printf("quit %s\n", holder->name);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  latencyStore = malloc(TIMES * sizeof(IUINT64));
+  buf = malloc(DATA_SIZE);
+  test_kcp(0, echo_kcp);
+  printLatency(latencyStore);
+  free(latencyStore);
+  free(buf);
+  return 0;
+}
